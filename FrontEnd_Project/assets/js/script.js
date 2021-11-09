06fef181775985b667d2db3f327abb31590ae6cc
PunhanSh <punhanash@code.edu.az>
2021-11-09 23:01:00 +0400

    Added Task

---
+++ FrontEnd_Project/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+      0:{
+        items: 1,
+      }
+    }
+    });
+  });
+
+ 
